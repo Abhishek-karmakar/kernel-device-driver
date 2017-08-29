26ac2f1ce057b28be2156e9b586dde24af423907
abhishek-karmakar <abhishek.karmakar.micromaxinfo.com>
2017-08-29 21:12:11 +0530

    adding the function signature from the linux/fs.h and expanding them

--- ex07_simple_module.c
+++ ex07_simple_module.c
@@ -9,10 +9,24 @@
 	int (*open) (struct inode *, struct file *);
 	int (*release) (struct inode *, struct file *);
 */
-ssize_t ex07_read (struct file *, char __user *, size_t, loff_t *);
-ssize_t ex07_write (struct file *, const char __user *, size_t, loff_t *);
-int ex07_open (struct inode *, struct file *);
-int ex07_close (struct inode *, struct file *);
+ssize_t ex07_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset)
+{
+
+}
+
+ssize_t ex07_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset)
+{
+
+}
+int ex07_open(struct inode *pinode, struct file *pfile)
+{
+
+}
+
+int ex07_close(struct inode *pinode, struct file *pfile)
+{
+
+}
 
 /*To hold the file operations performed on the device*/
 struct file_operations ex07_file_operations = {
